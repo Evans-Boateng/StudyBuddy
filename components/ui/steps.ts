f0cd5a99806b7e056eca841d56a121a67b68e491
Evans-Boateng <eb5337877@gmail.com>
2024-12-24 05:06:56 +0000

    Added the steps file

---
+++ components/ui/steps.ts
@@ -0,0 +1,26 @@
+export const features = [
+    {
+      title: "Sign Up or Log In",
+      description: "Create an account to start your learning journey. Gain instant access to study rooms and connect with fellow students."
+    },
+    {
+      title: "Explore Study Rooms",
+      description: "Browse through a variety of topics or use the search feature to find rooms that match your interests. There’s always a room for every challenge!"
+    },
+    {
+      title: "Create Your Own Room",
+      description: "Struggling with a topic? Set up your own study room and invite others to join. Collaborate with like-minded peers and conquer challenges together."
+    },
+    {
+      title: "Collaborate and Share Ideas",
+      description: "Engage in lively discussions, share valuable resources, and work as a team to find solutions. Learning is better together!"
+    },
+    {
+      title: "Direct Messaging",
+      description: "Need personal guidance? Send a direct message to another user and build meaningful one-on-one connections."
+    },
+    {
+      title: "Stay Organized",
+      description: "Keep track of all your activities, study rooms, and conversations from your profile. Your learning hub, all in one place."
+    }
+  ];
