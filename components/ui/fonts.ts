62e604c15238fff196b0b0d2859b11e28734805e
Evans-Boateng <eb5337877@gmail.com>
2024-12-25 03:31:05 +0000

    Added the inter font

--- components/ui/fonts.ts
+++ components/ui/fonts.ts
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { Inter } from "next/font/google";
 
 export const poppins = Poppins({
   variable: "--font-poppins",
@@ -10,4 +11,9 @@ export const plusJakartaSans = Plus_Jakarta_Sans({
   variable: "--font-plus-jakarta-sans",
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800"],
+})
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 })
